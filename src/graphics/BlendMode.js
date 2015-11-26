0bce989c0affaf5f12fae74411fccab84e7d16ba
JT <xxyellowsixx@gmail.com>
2015-11-25 23:16:41 -0800

    add BlendMode constants (will be used in the future)

---
+++ src/graphics/BlendMode.js
@@ -0,0 +1,21 @@
+const BlendMode = {
+	"Normal": 0,
+	"Add": 1,
+	"Multiply": 2,
+	"Screen": 3,
+	"Overlay": 4,
+	"Darken": 5,
+	"Lighten": 6,
+	"ColorDodge": 7,
+	"ColorBurn": 8,
+	"HardLight": 9,
+	"SoftLight": 10,
+	"Difference": 11,
+	"Exclusion": 12,
+	"Hue": 13,
+	"Saturation": 14,
+	"Color": 15,
+	"Luminosity": 16
+};
+
+export default BlendMode;
